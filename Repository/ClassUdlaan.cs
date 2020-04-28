852df54bc8e6a75e42e0e0e43dfde7ac365e7b75
Asger Hvam Dinesen <64129426+asgerhd@users.noreply.github.com>
2020-04-28 11:38:04 +0200

    fhsrjh

---
+++ Repository/ClassUdlaan.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ClassUdlaan : Notify
+    {
+		private DateTime _afleveringsDato;
+		public ClassUdlaan()
+		{
+		}
+
+
+		public DateTime afleveringsDato
+		{
+			get { return _afleveringsDato; }
+			set
+			{
+				if (_afleveringsDato != value)
+				{
+					_afleveringsDato = value;
+				}
+				Notify("afleveringsDato");
+			}
+		}
+
+		public void BeregnAfleveringsDato(DateTime lentdate, string userStatus)
+		{
+			if (userStatus == "normallåner")
+			{
+				DateTime afleveringsDato = lentdate.AddDays(30);
+			}
+			else
+			{
+				DateTime afleveringsDato = lentdate.AddDays(60);
+			}
+		}
+
+	}
+}
